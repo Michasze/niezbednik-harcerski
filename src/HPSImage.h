851f8058cbfe17f8a48f09cfae58de6c90b9ae32
Michasze <oskar_pazdziorko@protonmail.com>
2021-08-11 19:46:43 +0200

    HPSImage: use radius as parameter
    and enable antialiasing for round image

--- src/HPSImage.h
+++ src/HPSImage.h
@@ -22,22 +22,40 @@ public:
  QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override
 {
   int width = 400;
-  QImageReader reader(QString("://contents/ui/img/") + id, "jpg");
+  QString adres;
+  int radius = 0;
+  QStringList list = id.split(QLatin1Char(','), Qt::SkipEmptyParts);
+  if(list.size() == 2)
+    {
+        adres = list[0];
+        radius = list.at(1).toInt();
+    }
+  else {
+    adres = id;
+  }
+  QImageReader reader(QString("://contents/ui/img/") + adres, "jpg");
   QSize imgSize = reader.size();
   float ratio = (float) imgSize.width() / (float) imgSize.height();
   qInfo() << "ratio: " << ratio;
   int fixedHeight = (float) requestedSize.width() / ratio;
+  qInfo() << "fixed: " << fixedHeight;
   reader.setScaledSize(QSize(requestedSize.width() > 0 ? requestedSize.width() : width, fixedHeight > 0 ? fixedHeight : 400));
   QImage image(reader.read());
+  if(list.size() == 2)
+    {
   QImage out(image.width(), image.height(), QImage::Format_ARGB32);
   out.fill(Qt::transparent);
   QPainter painter(&out);
+  painter.setRenderHint(QPainter::Antialiasing);
   painter.setCompositionMode(QPainter::CompositionMode_Source);
-//  painter.fillRect(QRect(0,0, 2048, 1152), QColor("red"));
   painter.setBrush( image );
-  painter.drawRoundedRect(image.rect(), requestedSize.width() / 8, requestedSize.width() / 8);
+  painter.drawRoundedRect(image.rect(), radius, radius, Qt::RelativeSize);
   painter.end();
   return out;
+    }
+  else {
+    return image;
+  }
 }
 };
 
