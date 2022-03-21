4638e04fe2e18b2fa9aa15c90c4676a5ce5a1cf3
michaszek8 <oskar.pazdziorko@protonmail.com>
2022-03-21 09:57:46 +0100

    settings: fix indentation and change project name

--- src/HPSSettings.cpp
+++ src/HPSSettings.cpp
@@ -5,7 +5,7 @@
 HPSSettings::HPSSettings(QObject *parent)
     : QObject(parent)
 {
-  QSettings  m_settings("HPS", "Niezbędnik Harcerski");
+  QSettings  m_settings("HPS", "Niezbednik");
 #if (defined(Q_OS_ANDROID) || defined(Q_OS_IOS))
         isTabletModeAvailable = true;
          m_tabletMode= true;
@@ -29,36 +29,36 @@ HPSSettings::HPSSettings(QObject *parent)
 }
 
 void HPSSettings::neverToggle(const bool &a)
-  {
-    m_settings.setValue("showStartupPopup", a);
-  }
+{
+  m_settings.setValue("showStartupPopup", a);
+}
 
 bool HPSSettings::neverShowIsToggled()
 {
   return m_settings.value("showStartupPopup").toBool();
 }
 void HPSSettings::neverToggle2(const bool &a)
-  {
-    m_settings.setValue("showStartupPopup2", a);
-  }
+{
+  m_settings.setValue("showStartupPopup2", a);
+}
 
 bool HPSSettings::neverShow2IsToggled()
 {
   return m_settings.value("showStartupPopup2").toBool();
 }
 void HPSSettings::neverToggle3(const bool &a)
-  {
-    m_settings.setValue("showStartupPopup3", a);
-  }
+{
+  m_settings.setValue("showStartupPopup3", a);
+}
 
 bool HPSSettings::neverShow3IsToggled()
 {
   return m_settings.value("showStartupPopup3").toBool();
 }
 void HPSSettings::neverToggle4(const bool &a)
-  {
-    m_settings.setValue("showStartupPopup4", a);
-  }
+{
+  m_settings.setValue("showStartupPopup4", a);
+}
 
 bool HPSSettings::neverShow4IsToggled()
 {
@@ -66,9 +66,9 @@ bool HPSSettings::neverShow4IsToggled()
 }
 
 void HPSSettings::darkTheme(const bool &b)
-  {
-    m_settings.setValue("darkTheme", b);
-  }
+{
+  m_settings.setValue("darkTheme", b);
+}
 
 bool HPSSettings::darkThemeIsToggled()
 {
@@ -77,84 +77,84 @@ bool HPSSettings::darkThemeIsToggled()
 }
 
 void HPSSettings::setTextSize(const int &b)
-  {
-    qInfo() << b;
-    m_settings.setValue("textSize", b);
-  }
+{
+  qInfo() << b;
+  m_settings.setValue("textSize", b);
+}
 int HPSSettings::textSize()
-  {
-    return m_settings.value("textSize", 14).toInt();
-  }
+{
+  return m_settings.value("textSize", 14).toInt();
+}
 void HPSSettings::setSpiewnikTextSize(const int &c)
-  {
-    qInfo() << c;
-    m_settings.setValue("spiewnikTextSize", c);
-  }
+{
+  qInfo() << c;
+  m_settings.setValue("spiewnikTextSize", c);
+}
 int HPSSettings::spiewnikTextSize()
-  {
-    return m_settings.value("spiewnikTextSize", 14).toInt();
-  }
+{
+  return m_settings.value("spiewnikTextSize", 14).toInt();
+}
 void HPSSettings::setPakowanieId(const QString &d)
-  {
-    if (d != m_pakowanieId)
-      {
-        m_pakowanieId = d;
-        qInfo() << m_pakowanieId;
-      }
-  }
-QString HPSSettings::pakowanieId()
-  {
-    return m_pakowanieId;
-  }
-void HPSSettings::pakowanieToggle(const bool &e)
-  {
-    qInfo() << "set " << e;
-    m_settings.setValue(m_pakowanieId, e);
-  }
-bool HPSSettings::pakowanieIsToggled()
-  {
-    qInfo() << "get " << m_settings.value(m_pakowanieId);
-    return m_settings.value(m_pakowanieId).toBool();
-  }
-void HPSSettings::setIsMobile(const bool &f)
-  {
-    m_mobile = f;
-    isMobileChanged();
-  }
-bool HPSSettings::isMobile()
-  {
-#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS) || defined(UBUNTU_TOUCH)
-    m_mobile = true;
-#else
-    // Mostly for debug purposes and for platforms which are always mobile,
-    // such as Plasma Mobile
-    if (qEnvironmentVariableIsSet("QT_QUICK_CONTROLS_MOBILE")) {
-        m_mobile = QByteArrayList{"1", "true"}.contains(qgetenv("QT_QUICK_CONTROLS_MOBILE"));
-    } else {
-        m_mobile = false;
+{
+  if (d != m_pakowanieId)
+    {
+      m_pakowanieId = d;
+      qInfo() << m_pakowanieId;
     }
-#endif
-    return m_mobile;
+}
+QString HPSSettings::pakowanieId()
+{
+  return m_pakowanieId;
+}
+void HPSSettings::pakowanieToggle(const bool &e)
+{
+  qInfo() << "set " << e;
+  m_settings.setValue(m_pakowanieId, e);
+}
+bool HPSSettings::pakowanieIsToggled()
+{
+  qInfo() << "get " << m_settings.value(m_pakowanieId);
+  return m_settings.value(m_pakowanieId).toBool();
+}
+void HPSSettings::setIsMobile(const bool &f)
+{
+  m_mobile = f;
+  isMobileChanged();
+}
+bool HPSSettings::isMobile()
+{
+#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS) || defined(UBUNTU_TOUCH)
+  m_mobile = true;
+#else
+  // Mostly for debug purposes and for platforms which are always mobile,
+  // such as Plasma Mobile
+  if (qEnvironmentVariableIsSet("QT_QUICK_CONTROLS_MOBILE")) {
+    m_mobile = QByteArrayList{"1", "true"}.contains(qgetenv("QT_QUICK_CONTROLS_MOBILE"));
+  } else {
+    m_mobile = false;
   }
+#endif
+  return m_mobile;
+}
 QStringList HPSSettings::information() const
 {
-    return {
-        tr("Szkielety KDE %1").arg(QStringLiteral("5.90.0")),
-        tr("System okien %1").arg(QGuiApplication::platformName()),
-        tr("Qt %2 (zbudowany na %3)").arg(QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR))};
+  return {
+    tr("Szkielety KDE %1").arg(QStringLiteral("5.90.0")),
+    tr("System okien %1").arg(QGuiApplication::platformName()),
+    tr("Qt %2 (zbudowany na %3)").arg(QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR))};
 }
 void HPSSettings::setTransientTouchInput(bool touch)
 {
-    if (touch == m_hasTransientTouchInput) {
-        return;
-    }
+  if (touch == m_hasTransientTouchInput) {
+    return;
+  }
 
-    m_hasTransientTouchInput = touch;
-    if (!m_tabletMode) {
-        Q_EMIT hasTransientTouchInputChanged();
-    }
+  m_hasTransientTouchInput = touch;
+  if (!m_tabletMode) {
+    Q_EMIT hasTransientTouchInputChanged();
+  }
 }
 bool HPSSettings::hasTransientTouchInput() const
 {
-    return m_hasTransientTouchInput || m_tabletMode;
+  return m_hasTransientTouchInput || m_tabletMode;
 }
